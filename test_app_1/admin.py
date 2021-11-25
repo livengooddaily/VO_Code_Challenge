d5e68145f45f1575b3a25cefdc239b552642096a
Eddie Livengood <eddie.livengood@gmail.com>
2021-11-25 17:27:08 +0100

    models registered in admin site and django debug toolbar added for easy query analysis

--- test_app_1/admin.py
+++ test_app_1/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from test_app_1.models import SKU, Warehouse, Purchase_Order, Plain_Carton_Line_Item
+
+admin.site.register(SKU)
+admin.site.register(Warehouse)
+admin.site.register(Purchase_Order)
+admin.site.register(Plain_Carton_Line_Item)
--- vo_test_code/urls.py
+++ vo_test_code/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,4 +22,5 @@ app_name = 'test_app_1'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('test_app_1.urls', 'test_app_1'), namespace='test_app_1')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
